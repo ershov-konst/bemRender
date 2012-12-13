7c21443772a108a1cffeabdf51cb3f2a393263a7
ershovk <ershov.konst@gmail.com>
2012-12-13 15:47:19 +0400

    тестовые данные

--- example/mvc/controllers/index.js
+++ example/mvc/controllers/index.js
@@ -11,16 +11,16 @@ module.exports = {
          text : "<h1>index page</h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
          tableData : [
             {
-               first : "aaaa",
-               second : "bbbb"
+               "first" : "aaaa",
+               "second" : "bbbb"
             },
             {
-               first : "ccccc",
-               second : "dddd"
+               "first" : "ccccc",
+               "second" : "dddd"
             },
             {
-               first : "eeeee",
-               second : "fffff"
+               "first" : "eeeee",
+               "second" : "fffff"
             }
          ]
       };
